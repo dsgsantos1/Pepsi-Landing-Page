81f42967fc904304969e0185e3535c82d70bbe91
dsgsantos1 <dsgsantos1@gmail.com>
2022-04-23 16:17:29 -0400

    Add home

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function imgSlider(scr){
+    document.querySelector('.pepsi').src = scr;
+}
+
+function changeBackgroundColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
